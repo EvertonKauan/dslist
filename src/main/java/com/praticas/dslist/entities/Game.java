a09f650af76ca2e1b6ec469fafe09abc6af176af
EvertonKauan <everton.kauan.silva.oliveira@ccc.ufcg.edu.br>
2023-05-14 17:05:01 -0300

    Game seed

--- src/main/java/com/praticas/dslist/entities/Game.java
+++ src/main/java/com/praticas/dslist/entities/Game.java
@@ -15,21 +15,23 @@ public class Game {
     private String genre;
     private String platforms;
     private Double score;
-    private String shortDescription;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
-    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String shortDescription, String imgUrl, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
         this.score = score;
-        this.shortDescription = shortDescription;
         this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
 
@@ -81,20 +83,20 @@ public class Game {
         this.score = score;
     }
 
-    public String getShortDescription() {
-        return shortDescription;
-    }
-
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
-
     public String getImgUrl() {
         return imgUrl;
     }
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
     }
 
     public String getLongDescription() {
